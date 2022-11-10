ae332a0f4b6fb858cd463e9bc4d70d6dbd047df5
FreeSpirit0 <poomtum2544@hotmail.com>
2022-11-10 13:29:39 +0700

    fix thing so eslint can shutup

--- pages/charts/candidate.js
+++ pages/charts/candidate.js
@@ -31,18 +31,18 @@ const Candidate = () => {
         <h1 className={styles.title}>Score for each candidate</h1>
         
 				{[...Array(10).keys()].map(i => i + 1).map(i => 
-					<div className={styles.grid}>
-					<div className={styles.card}>
-						<Image 
-							src="https://ih1.redbubble.net/image.430094232.0384/flat,800x800,075,f.u2.jpg" 
-							alt="Dog"
-							width={100}
-							height={100}/>
-						<p>อันดับ {i}</p>
-						<h2>พรรคหมา</h2>
-						<p>คะแนนรวม {Math.floor(100 / i)} เสียง</p>
-					</div>
-				</div>)}
+					<div className={styles.grid} key={i}>
+						<div className={styles.card}>
+							<Image 
+								src="https://ih1.redbubble.net/image.430094232.0384/flat,800x800,075,f.u2.jpg" 
+								alt="Dog"
+								width={100}
+								height={100}/>
+							<p>อันดับ {i}</p>
+							<h2>พรรคหมา</h2>
+							<p>คะแนนรวม {Math.floor(100 / i)} เสียง</p>
+						</div>
+					</div>)}
 				
       </main>
     </div>
