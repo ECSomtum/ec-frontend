c0888f86dfafb0ff885aa767a2dc7043f5206593
FreeSpirit0 <poomtum2544@hotmail.com>
2022-11-11 12:43:33 +0700

    add loading when fetching so I can become a better person

--- pages/charts/candidate.js
+++ pages/charts/candidate.js
@@ -4,46 +4,61 @@ import styles from "../../styles/Home.module.css";
 import Image from "next/image";
 import axios from "axios";
 import { WhoCaresAboutNamingThingsBro } from "../../utils/utils";
+import { CircularProgress } from "@mui/material";
 
 const Candidate = () => {
-	const [party, setParty] = useState([])
+  const [party, setParty] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:8000/parties")
+      .then((response) => response.data)
+      .then((data) => {
+        setParty(data);
+        setLoading(false);
+      })
+      .catch((_) => {
+        const mockParty = WhoCaresAboutNamingThingsBro();
+        setParty(mockParty);
+		setLoading(false)
+      });
+  }, []);
 
-	useEffect(() => {
-    axios.get("http://localhost:8000/parties")
-    .then(response => response.data)
-    .then(data => setParty(data))
-	.catch(_ => {
-		const mockParty = WhoCaresAboutNamingThingsBro()
-		setParty(mockParty)
-	})
-  },[])
+  if (loading)
+    return (
+      <div className={styles.main}>
+        <CircularProgress />
+      </div>
+    );
+  else
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Score for each candidate</title>
+        </Head>
 
-	return (
-    <div className={styles.container}>
-      <Head>
-        <title>Score for each candidate</title>
-      </Head>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Score for each candidate</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Score for each candidate</h1>
-        
-				{party.map(p => 
-					<div className={styles.grid} key={p.id}>
-						<div className={styles.card}>
-							<Image 
-								src={p.pictureUrl} 
-								alt={"party" + p.id}
-								width={100}
-								height={100}/>
-							<p>อันดับ {p.id}</p>
-							<h2>{p.name}</h2>
-							<p>คะแนนรวม {Math.floor(100 / p.id)} เสียง</p>
-						</div>
-					</div>)}
-				
-      </main>
-    </div>
-  );
+          {party.map((p) => (
+            <div className={styles.grid} key={p.id}>
+              <div className={styles.card}>
+                <Image
+                  src={p.pictureUrl}
+                  alt={"party" + p.id}
+                  width={100}
+                  height={100}
+                />
+                <p>อันดับ {p.id}</p>
+                <h2>{p.name}</h2>
+                <p>คะแนนรวม {Math.floor(100 / p.id)} เสียง</p>
+              </div>
+            </div>
+          ))}
+        </main>
+      </div>
+    );
 };
 
 export default Candidate;
