803110fdf85958e3a65f09861a83827cfb2ee3b7
kikikrittamate <kikikrittamate@gmail.com>
2022-12-02 02:00:28 +0700

    add homepage text test

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -3,6 +3,10 @@ describe('ec dashboard', () => {
     cy.visit('http://localhost:3000')
   })
 
+  it('render homepage text', () =>{
+    cy.contains('All Election Dashboard').should('be.visible')
+  })
+  
   it('display home page with all link to dashboard', () => {
     cy.get('a[href="/charts/party"]').should('have.length', 1)
     cy.get('a[href="/charts/mostvoted/party"]').should('have.length', 1)
